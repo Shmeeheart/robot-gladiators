5a6aec8ff2ac8548f222e25a2c27a3605eccd822
Shmeeheart <Shmeeheart.goodwije@gmail.com>
2022-05-07 14:14:07 -0600

    editing script

--- assets/js/game.js
+++ assets/js/game.js
@@ -99,6 +99,7 @@ else {
   if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+    debugger; 
 
     // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
@@ -182,4 +183,4 @@ var shop = function() {
 }
 };
 //start the game when the page loads
-startGame ();
+//startGame ();
